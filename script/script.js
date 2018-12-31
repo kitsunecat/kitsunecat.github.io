4de369dbaa9fb67aab0fe274dec2ef69908d771b
kitsunecat <scooping.fox@gmail.com>
2019-01-01 00:20:45 +0900

    あいさつした

--- script/script.js
+++ script/script.js
@@ -1,10 +1,18 @@
 // jQuery
 $(document).ready(function(){
+lastNo = '';
 
 //workのサムネイルをクリックしたら詳細をスライドして開く
   $('.work-thum').on('click', function(){
-    var workNo = '#work' + $(this).attr("id");  //work番号を取得
-    $('.work').hide('fast'); //開いているworkを閉じる
-    $(workNo).show('fast'); //開いているworkを閉じる
+    $('.work').hide('fast');
+    var workNo = '#work' + $(this).attr("id");
+    if (workNo === lastNo) {
+      console.log(workNo);
+      console.log(lastNo);
+      return;
+    }
+    $(workNo).show('fast');
+    $("html,body").animate({scrollTop:$('#works').offset().top});
+    lastNo = workNo;
   })
 })
